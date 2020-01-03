c574b14acba11d5993720fc656059f3584b16671
lmunck <anders@lmunck.com>
2020-01-03 21:18:20 +0100

    Initial Commit

---
+++ SwiftUIChallenges/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChallenges
+//
+//  Created by Anders Munck on 03/01/2020.
+//  Copyright © 2020 Anders Munck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
